ad552f447c31cd0130eb21ccc4768d9ffd2c28e4
eefano <77832+eefano@users.noreply.github.com>
2024-10-30 20:25:52 +0100

    no hash in filenames

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,13 @@
 export default {
-    base: '/dungcraw24/'
-  }
+  base: "/dungcraw24/",
+
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`,
+      },
+    },
+  },
+};
